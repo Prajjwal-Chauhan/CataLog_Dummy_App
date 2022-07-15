3805e257e6686956c2270b6701382b7f45ede295
Prajjwal <prajjwalchauhan94017@gmail.com>
2022-07-15 13:26:10 +0530

    Day 3 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/home_page.dart';
+import 'package:flutter_application_1/pages/home_page.dart';
+import 'package:flutter_application_1/pages/login_page.dart';
 void main(List<String> args) {
   runApp(MyApp());
 }
@@ -10,11 +11,21 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
-    int days = 30;
-    String name = "Prajjwal";
     return MaterialApp(
-      home: HomePage()
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark
+      ),
+      initialRoute: "/home",
+      routes: {
+        "/" : (context) => LoginPage(),
+        "/home" : (context) => HomePage(),
+        "/login" : (context) => LoginPage()
+      },
     );
+  
   }
 }
