4f9c9d789341a7bf06933c7964766579969fd9be
Prajjwal <prajjwalchauhan94017@gmail.com>
2022-07-15 17:19:25 +0530

    Day 4 Complete

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -6,16 +6,57 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-          child: Text(
-        "Login Page",
-        style: TextStyle(
-          fontSize: 40, 
-          color: Colors.deepPurple,
-          fontWeight: FontWeight.bold
-          ),
-      )
-      ),
-    );
+        color: Colors.white,
+        child: Column(
+          children: [
+            Image.asset(
+              "assets/images/Login_image.png", 
+              fit: BoxFit.cover
+              ),
+            SizedBox(
+              height:20.0
+            ),
+            Text(
+              "Welcome",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+
+              ),
+            ),
+            SizedBox(
+              height:20.0
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal : 32.0,vertical : 16.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                    hintText: "Enter username",
+                    labelText: "Username"
+                  )
+                ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                    hintText: "Enter Password",
+                    labelText: "Password" 
+                  )
+                ),
+                SizedBox(
+                  height:20.0
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    print("Hi Prajjwal");
+                  },
+                  child: Text("Login")
+                  )
+              ],
+              ),
+            )
+        ],
+      ));
   }
 }
